732aad8ade85975a4c9e77b67592687070e57d32
jpramirezo <35973400+jpramirezo@users.noreply.github.com>
2018-05-22 14:15:24 -0500

    Update sketch.js

--- 11/sketch.js
+++ 11/sketch.js
@@ -190,7 +190,7 @@ function draw() {
     }
 
     // Si el la represa se completa ganas 
-    if (radio <= 300) {
+    if (radio <= 250) {
       estado = WIN;
     }
 
@@ -440,9 +440,9 @@ function osos(_genero) {
       dummy1 = abs(this.dirY);
 
       //Si el radio del lago es menor a 400 los osos se mueven más rápido 
-      if (radio <= 450) {
-        this.dirX = (this.dirX / dummy) * random(3, 4.5);
-        this.dirY = (this.dirY / dummy1) * random(3, 4.5);
+      if (radio <= 350) {
+        this.dirX = (this.dirX / dummy) * random(3.5, 5);
+        this.dirY = (this.dirY / dummy1) * random(3.5, 5);
       } else {
         this.dirX = (this.dirX / dummy) * random(2, 3.5);
         this.dirY = (this.dirY / dummy1) * random(2, 3.5);
